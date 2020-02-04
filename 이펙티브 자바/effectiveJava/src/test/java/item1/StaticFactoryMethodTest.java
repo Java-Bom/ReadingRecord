5567948914a315efe14465bf25cc3c38eca0354d
csbsjy <seojaeyeon@lotte.net>
2020-02-04 19:45:34 +0900

    [item1,7]  예제 작성

---
+++ 이펙티브 자바/effectiveJava/src/test/java/item1/StaticFactoryMethodTest.java
@@ -0,0 +1,20 @@
+package item1;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+public class StaticFactoryMethodTest {
+
+    @DisplayName("Boolean은 불변클래스이다.")
+    @Test
+    void booleanTest(){
+        Boolean boolean1 = Boolean.valueOf(true);  // = Boolean.TRUE
+        Boolean boolean2 = Boolean.valueOf("true");
+        Boolean boolean3 = new Boolean(true);
+
+        assertThat(boolean1).isSameAs(boolean2);
+        assertThat(boolean1).isNotSameAs(boolean3);
+    }
+}
