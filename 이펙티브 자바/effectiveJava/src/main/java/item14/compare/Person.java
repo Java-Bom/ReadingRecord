84454abceb9aed53cd3f3f5d58914cc5c10fc180
최유성 <chldbtjd7530@naver.com>
2020-02-06 23:29:01 +0900

    treeset 예제

--- 이펙티브 자바/effectiveJava/src/main/java/item14/compare/Person.java
+++ 이펙티브 자바/effectiveJava/src/main/java/item14/compare/Person.java
@@ -1,4 +1,26 @@
 package item14.compare;
 
-public class Person {
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+public class Person implements Comparable<Person> {
+
+    private Integer age;
+    private String name;
+
+    @Override
+    public int compareTo(Person o) {
+        int result = Integer.compare(age, o.age);
+        if (result == 0) {
+            result = name.compareTo(o.name);
+        }
+
+        return result;
+    }
 }
