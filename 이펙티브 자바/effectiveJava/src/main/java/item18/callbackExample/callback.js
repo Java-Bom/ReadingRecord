dfe255bce4fb0f840a40bbd3d64f8c3c5663d147
mjung1798 <mor2222@naver.com>
2020-02-29 15:53:29 +0900

    callback js example

--- 이펙티브 자바/effectiveJava/src/main/java/item18/callbackExample/callback.js
+++ 이펙티브 자바/effectiveJava/src/main/java/item18/callbackExample/callback.js
@@ -1,20 +1,32 @@
 function add(x, callback){
     let sum = x + x;
-    console.log(sum);
     callback(sum);
 }
 
-let out_string = "hello";
+function minus(x, callback){
+    let sum = x - x;
+    callback(sum);
+}
+
 add(3, function(result){
-    add(result, function (result2) {
-        console.log(result);
-        console.log(result2);
-        console.log(out_string);
-        add(result2, function (result3) {
-            console.log(result);
-            console.log(result2);
-            console.log(result3);
-            console.log("hi" + result3);
-        })
-    })
+    console.log("=========add1");
+    console.log(this);
+    var self = this;
+
+    setTimeout(function(){
+
+        console.log("---------------this");
+        console.log(this);
+
+        console.log("---------------setTimeout self");
+        console.log(self);
+
+    },200);
+
+});
+
+minus(3, function (result) {
+    console.log("---------------minus");
+    console.log(this);
+    console.log(self);
 });
