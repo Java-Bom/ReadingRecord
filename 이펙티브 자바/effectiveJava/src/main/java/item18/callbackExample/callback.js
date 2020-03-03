2caf5a068795df2e77e0e7f83487d985721d929e
mjung1798 <mor2222@naver.com>
2020-03-03 23:43:12 +0900

    Refactor: This에 대한 고찰
    This는 객체를 가르키는게 맞음, 이전에 했던 예제에서의 this는 당연히, 전역 메서드를 선언했으니 this를 찍어본 결과가 Global Object인게 당연했다.
    setInterval이 this가 아닌 setInterval을 찍은 이유는 setInterval 함수가 메서드를 부르면 객체를 선언하고, setInterval을 실행하기 때문이다.
    고로, var나 class선언을 이용해 새로운 객체를 만들어 보고, 그 객체의 함수에서 this를 찍어본 결과 Object가 나온다.
    야호~ 삽질 끝

--- 이펙티브 자바/effectiveJava/src/main/java/item18/callbackExample/callback.js
+++ 이펙티브 자바/effectiveJava/src/main/java/item18/callbackExample/callback.js
@@ -8,25 +8,61 @@ function minus(x, callback){
     callback(sum);
 }
 
+const Rectangle = class {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+    area() {
+        console.log("rectangle========");
+        console.log(this);
+        return this.height * this.width;
+    }
+};
+
 add(3, function(result){
     console.log("=========add1");
     console.log(this);
     var self = this;
-
     setTimeout(function(){
 
-        console.log("---------------this");
-        console.log(this);
+        console.log("---------------this"+this);
+        console.log();
 
         console.log("---------------setTimeout self");
         console.log(self);
 
     },200);
-
 });
 
-minus(3, function (result) {
-    console.log("---------------minus");
+
+minus(3, function () {
+    console.log("minus3:========");
     console.log(this);
-    console.log(self);
+    new Rectangle(5,8).area();
+    function hello(callback){
+        callback();
+    }
+    hello(function () {
+        console.log("hello:=====");
+        console.log(this);
+    })
 });
+
+
+var obj = {
+    name: "minjeong",
+    function(){
+        console.log("hello");
+        console.log(this);
+    },
+    helloFunction : function(){
+        console.log("hello function");
+        console.log(this);
+    }
+};
+
+console.log("obj=====");
+obj.function();
+obj.helloFunction();
+console.log(obj.name);
