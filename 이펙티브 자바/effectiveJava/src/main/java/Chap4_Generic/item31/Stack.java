a2348db27062162158f9d0c369d06c2afcef2078
csbsjy <seojaeyeon@lotte.net>
2020-03-28 12:53:08 +0900

    [item 31]

---
+++ 이펙티브 자바/effectiveJava/src/main/java/Chap4_Generic/item31/Stack.java
@@ -0,0 +1,24 @@
+package Chap4_Generic.item31;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class Stack<E> {
+    private List<E> list = new ArrayList<>();
+
+    public void pushAll(Iterable<? extends E> src) {
+        for (E e : src) {
+            push(e);
+        }
+    }
+
+    public void push(E e) {
+        list.add(e);
+    }
+
+    public void popAll(Collection<? super E> dst) {
+        dst.addAll(list);
+        list.clear();
+    }
+}
