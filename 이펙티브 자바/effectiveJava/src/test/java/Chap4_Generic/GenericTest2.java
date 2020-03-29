1b38f8d30570263561553982069a02945bd444b6
최유성 <chldbtjd7530@naver.com>
2020-03-30 02:50:40 +0900

    비검사 경고 md 추가

--- 이펙티브 자바/effectiveJava/src/test/java/Chap4_Generic/GenericTest2.java
+++ 이펙티브 자바/effectiveJava/src/test/java/Chap4_Generic/GenericTest2.java
@@ -3,7 +3,9 @@ package Chap4_Generic;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class GenericTest2 {
@@ -18,16 +20,26 @@ public class GenericTest2 {
         Set<Long> num2s = new HashSet<>();
         Set<String> num3s = new HashSet<>();
 
-        count(num1s,num2s);
+        count(num1s, num2s);
         //count(num1s,num3s);
-        count2(num1s,num1s);
+        count2(num1s, num1s);
         //count2(num1s,num2s);
+
+    }
+
+    @Test
+    @DisplayName("다이아 몬드는 타입추론이 가능하게 도와준다.")
+    void name3() {
+
+        @SuppressWarnings("unchecked")
+        List<String> strings = new ArrayList();
+        List<String> strings2 = new ArrayList<>();
 
     }
 
     private void count(Set<? extends Number> source, Set<? extends Number> destination) {
     }
 
-    private <E extends Number> void count2(Set<E> source,Set<E> destination) {
+    private <E extends Number> void count2(Set<E> source, Set<E> destination) {
     }
 }
