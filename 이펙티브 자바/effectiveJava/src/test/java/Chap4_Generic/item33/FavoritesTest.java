124de15544c790a5684b8ee52b03df5298b9f53e
mjung1798 <mor2222@naver.com>
2020-04-15 15:15:29 +0900

    Refactor : Rewrite Issue Code
    이슈 코드를 재 작성하였다.

--- 이펙티브 자바/effectiveJava/src/test/java/Chap4_Generic/item33/FavoritesTest.java
+++ 이펙티브 자바/effectiveJava/src/test/java/Chap4_Generic/item33/FavoritesTest.java
@@ -1,7 +1,12 @@
 package Chap4_Generic.item33;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -14,4 +19,23 @@ class FavoritesTest {
         favorites.putFavorite((Class) Integer.class, "Integer인테 String instance로 넘겨 보자!");
         favorites.getFavorite(Integer.class);
     }
+
+    @Test
+    @DisplayName("HashSet Raw Type")
+    void name2() {
+        HashSet<Integer> set = new HashSet<>();
+        ((HashSet) set).add("문자열 입니다.");
+    }
+
+    @Test
+    @DisplayName("HashSet Raw Type")
+    void name3() {
+        HashSet<Integer> set = new HashSet<>();
+        ((HashSet)set).add("문자열 입니다.");
+        boolean contains1 = ((HashSet) set).contains("문자열 입니다.");
+        assertThat(contains1).isTrue();
+        boolean contains = set.contains("문자열 입니다.");
+        assertThat(contains).isTrue();
+    }
+
 }
