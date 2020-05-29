f6534281de973eaadd8cede927a746833eb49c00
mjung1798 <mor2222@naver.com>
2020-05-29 22:43:56 +0900

    Feat: chapter 8 method

---
+++ 이펙티브 자바/effectiveJava/src/main/java/Chap8_Method/item49/Assertion.java
@@ -0,0 +1,11 @@
+package Chap8_Method.item49;
+
+/**
+ * Created by jyami on 2020/05/29
+ */
+public class Assertion {
+    public static void main(String[] args) {
+        assert false;
+        System.out.println("hello!");
+    }
+}
