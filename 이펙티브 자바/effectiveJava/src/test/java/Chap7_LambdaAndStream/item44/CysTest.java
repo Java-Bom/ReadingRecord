f4752ee13fe6bd0a24b28085a23649489fa062a6
최유성 <chldbrjd@gmail.com>
2020-07-04 03:00:13 +0900

    아이템 43~44추가

--- 이펙티브 자바/effectiveJava/src/test/java/Chap7_LambdaAndStream/item44/CysTest.java
+++ 이펙티브 자바/effectiveJava/src/test/java/Chap7_LambdaAndStream/item44/CysTest.java
@@ -1,11 +1,14 @@
 package Chap7_LambdaAndStream.item44;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.*;
+import java.util.function.*;
 
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.partitioningBy;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class CysTest {
 
@@ -20,6 +23,28 @@ public class CysTest {
         Map<Boolean, List<String>> map2 = test.stream().collect(partitioningBy(Objects::isNull));
 
         //then
+    }
+
+    @Test
+    @DisplayName("표준함수형 인터페이스")
+    void name2() {
+        //
+        UnaryOperator<Integer> unaryOperator = x->x+1;
+        assertThat(unaryOperator.apply(1)).isEqualTo(2);
+        //
+        BinaryOperator<Integer> binaryOperator = (x,y)->x+y;
+        assertThat(binaryOperator.apply(1,2)).isEqualTo(3);
+        //
+        Predicate<Integer> predicate = x-> x==1;
+        assertThat(predicate.test(1)).isTrue();
+        //
+        Function<Integer,String> function = (x) -> String.valueOf(x);
+        assertThat(function.apply(1)).isEqualTo("1");
+        //
+        Supplier<Integer> supplier = ()->1;
+        assertThat(supplier.get()).isEqualTo(1);
+        //
+        Consumer<Integer> consumer = x ->{};
 
     }
 
