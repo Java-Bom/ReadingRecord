dac779e51f8bb7c7946eb17f5d85546a049cc5ba
mjung1798 <mor2222@naver.com>
2020-11-01 13:55:40 +0900

    Package: init toby-spring project

--- SpringToby/chapter1/src/main/java/TemplateMethodPattern.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by jyami on 2020/11/01
- */
-public class TemplateMethodPattern {
-}
---
+++ spring-toby/spring-project/src/main/java/com/javabom/toby/TobyApplication.java
@@ -0,0 +1,14 @@
+package com.javabom.toby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by jyami on 2020/11/01
+ */
+@SpringBootApplication
+public class TobyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TobyApplication.class, args);
+    }
+}
