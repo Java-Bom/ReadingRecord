c78310492e92a97446f72f2f3912e180a699060c
jaeyeon.seo <jaeyeon.seo@navercorp.com>
2020-11-07 12:21:46 +0900

    add: 의존관계 주입의 세가지 조건

--- spring-toby/spring-project/src/main/java/com/javabom/toby/chapter1/term/IoC_용어정리/ChapterOneConfiguration.java
+++ spring-toby/spring-project/src/main/java/com/javabom/toby/chapter1/term/IoC_용어정리/ChapterOneConfiguration.java
@@ -44,4 +44,17 @@ public class ChapterOneConfiguration {
     public UserRepository sessionUserRepository() {
         return new DefaultUserRepository();
     }
+
+    /*
+    UserService 는 이 설정 파일을 읽을 때(런타임)에 DefaultUserRepository 와 런타임 의존관계를 가진다.
+    여기서 DefaultUserRepository를 의존 오브젝트라고한다.(실제 사용대상의 오브젝트)
+    의존 관계 주입 특징 2) 런타임 시점의 의존관계는 컨테이너나 팩토리 같은 제 3의 존재가 결정한다.(여기서는 설정파일) --> 핵심
+     */
+    @Bean
+    public UserService userService() {
+        /*
+        의존관계 주입 특징 3) 사용할 오브젝트에 대한 레퍼런스 -> defaultUserRepository 는 외부에서 주입(생성자, Setter,,)
+         */
+        return new UserService(defaultUserRepository());
+    }
 }
