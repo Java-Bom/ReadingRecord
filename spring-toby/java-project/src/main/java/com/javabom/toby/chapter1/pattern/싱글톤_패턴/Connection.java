8baf61192a6c7feb5ef95c0dad7c25b7b34e3c71
jaeyeon.seo <jaeyeon.seo@navercorp.com>
2020-11-07 17:44:00 +0900

    update: 싱글턴의 한계 수정

---
+++ spring-toby/java-project/src/main/java/com/javabom/toby/chapter1/pattern/싱글톤_패턴/Connection.java
@@ -0,0 +1,5 @@
+package com.javabom.toby.chapter1.pattern.싱글톤_패턴;
+
+public interface Connection {
+    Object execute(String sql);
+}
--- spring-toby/java-project/src/main/java/com/javabom/toby/chapter1/pattern/싱글톤_패턴/UserDao.java
+++ spring-toby/java-project/src/main/java/com/javabom/toby/chapter1/pattern/싱글톤_패턴/UserDao.java
@@ -9,28 +9,32 @@ import java.util.Objects;
  * 3. 상속불가, static 필드 -> 객체지향 개념을 적용할 수 없음
  * 4. Mock 인스턴스 생성 불가. 테스트 불가.
  */
-public class Calculator {
+public class UserDao {
     private static final Object LOCK = new Object();
-    private static Calculator INSTANCE;
+    private static UserDao INSTANCE;
+    private final Connection connection;
 
-    private Calculator() {
+    public UserDao(Connection connection) {
+        this.connection = connection;
     }
 
     /**
-     * Calculator 객체의 책임은 계산하는 것인데
+     * UserDao 객체의 책임은 User모델을 다루는 로직들로만 응집되어있어야하는데
      * 책임과 전혀 무관한 싱글톤만을 위한 코드가 들아간다.
      */
-    public static Calculator getInstance() {
+    public static UserDao getInstance(Connection connection) {
         synchronized (LOCK) {
             if (Objects.isNull(INSTANCE)) {
-                INSTANCE = new Calculator();
+                INSTANCE = new UserDao(connection);
             }
         }
 
         return INSTANCE;
     }
 
-    public int add(int a, int b) {
-        return a + b;
+    public User getUser() {
+        // connection 에서 user얻어옴
+        Object result = connection.execute("select * from user");
+        return (User) result;
     }
 }
