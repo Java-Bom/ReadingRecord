6f88a8c1cd1bfa7fd8e9894de61a81a62a6ec999
Gobukgol <zxczc159@gmail.com>
2020-11-12 17:26:08 +0900

    feat(applicationContext.xml): DaoFactory 대신 xml 을 이용한 DI 설정
    GenericXmlApplicationContext, ClassPathXmlApplicationContext 방식.
    GenericXmlApplicationContext 방식은 파일 못찾아서 일단 보류

--- spring-toby/spring-project/src/main/java/com/javabom/toby/userdao/UserDaoConnectionTest.java
+++ spring-toby/spring-project/src/main/java/com/javabom/toby/userdao/UserDaoConnectionTest.java
@@ -1,13 +1,47 @@
 package com.javabom.toby.userdao;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import java.net.URL;
+import java.net.URLClassLoader;
 
 public class UserDaoConnectionTest {
     public static void main(String[] args) {
+        ClassLoader cl = ClassLoader.getSystemClassLoader();
+
+        URL[] urls = ((URLClassLoader) cl).getURLs();
+
+        for (URL url : urls) {
+            System.out.println(url.getFile());
+        }
+
+        /**
+         * DaoFactory를 설정정보로 사용하는 방법
+         */
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao annotationUserDao = context.getBean("userDao", UserDao.class);
+        System.out.println("---------------");
+        annotationUserDao.printConnectionMaker();
+        /**
+         * GenericXmlApplicationContext
+         */
 
-        UserDao userDao = context.getBean("userDao", UserDao.class);
+        /*
+        ApplicationContext genericXmlContext = new GenericXmlApplicationContext("");
+        UserDao genericUserDao = genericXmlContext.getBean("userDao", UserDao.class);
+        System.out.println("---------------");
+        genericUserDao.printConnectionMaker();
+        */
+        /**
+         * ClassPathXmlApplicationContext
+         */
+        ApplicationContext classPathContext = new ClassPathXmlApplicationContext("applicationContext.xml", UserDao.class);
+        UserDao classPathUserDao = classPathContext.getBean("userDao", UserDao.class);
+        System.out.println("---------------");
+        classPathUserDao.printConnectionMaker();
 
-        userDao.printConnectionMaker();
     }
 }
