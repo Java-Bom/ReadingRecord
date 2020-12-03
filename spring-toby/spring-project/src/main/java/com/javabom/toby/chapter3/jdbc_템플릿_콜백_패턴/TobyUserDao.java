9b599443593292132ab6e370f1fe5f4cb6d44ccc
csbsjy <seojaeyeon@lotte.net>
2020-12-03 22:17:27 +0900

    add: 네거티브 테스트

--- spring-toby/spring-project/src/main/java/com/javabom/toby/chapter3/jdbc_템플릿_콜백_패턴/TobyUserDao.java
+++ spring-toby/spring-project/src/main/java/com/javabom/toby/chapter3/jdbc_템플릿_콜백_패턴/TobyUserDao.java
@@ -1,20 +1,21 @@
 package com.javabom.toby.chapter3.jdbc_템플릿_콜백_패턴;
 
-import com.javabom.toby.userdao.User;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
 
-@Repository
-public class UserDao {
+public class TobyUserDao {
     private JdbcTemplate jdbcTemplate;
 
     public void setDataSource(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    public void add(User user) {
-        jdbcTemplate.update("INSERT INTO db_user.user VALUES (?, ?, ?)", user.getId(), user.getName(), user.getPassword());
+    public void add(TobyUser user) {
+        jdbcTemplate.update("INSERT INTO TOBY_USER VALUES (?, ?, ?)", user.getId(), user.getName(), user.getPassword());
+    }
+
+    public void get(final String unknown) {
+
     }
 }
---
+++ spring-toby/spring-project/src/test/java/com/javabom/toby/chapter3/jdbc_템플릿_콜백_패턴/TobyUserDaoTest.java
@@ -0,0 +1,26 @@
+package com.javabom.toby.chapter3.jdbc_템플릿_콜백_패턴;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+@ContextConfiguration("/applicationContext.xml")
+class TobyUserDaoTest {
+    @Autowired
+    TobyUserDao tobyUserDao;
+
+    @DisplayName("유저 저장테스트")
+    @Test
+    void add() {
+        /*
+         * 네거티브 테스트
+         * 예외상항에 대한 테스트
+         */
+        tobyUserDao.add(null);
+        tobyUserDao.get("unknown");
+    }
+
+}
