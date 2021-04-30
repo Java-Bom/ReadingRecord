b4a146efc04d2a59a13dcdf3afb067d2fb3e8e84
jaeyeon.seo <jaeyeon.seo@navercorp.com>
2021-04-30 17:07:15 +0900

    트랜잭션 테스트

--- spring-toby/spring-project/src/main/java/com/javabom/toby/chapter7/내장형_db/DbConfig.java
+++ spring-toby/spring-project/src/main/java/com/javabom/toby/chapter7/내장형_db/DbConfig.java
@@ -3,13 +3,19 @@ package com.javabom.toby.chapter7.내장형_db;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.transaction.support.TransactionTemplate;
 
 @Configuration
 public class DbConfig {
 
+	/**
+	 * 내장형 db Builder 를 사용한 EmbeddedDatabase
+	 * DataSource 를 상속
+	 */
 	@Bean
 	public EmbeddedDatabase embeddedDatabase() {
 		return new EmbeddedDatabaseBuilder()
@@ -21,5 +27,13 @@ public class DbConfig {
 	@Bean
 	public JdbcTemplate template() {
 		return new JdbcTemplate(embeddedDatabase());
+	}
+
+	/**
+	 * 트랜잭션을 보장을 위한 Template
+	 */
+	@Bean
+	public TransactionTemplate transactionTemplate() {
+		return new TransactionTemplate(new DataSourceTransactionManager(embeddedDatabase()));
 	}
 }
--- spring-toby/spring-project/src/test/java/com/javabom/toby/chapter7/내장형db/H2DbTest.java
+++ spring-toby/spring-project/src/test/java/com/javabom/toby/chapter7/내장형db/H2DbTest.java
@@ -4,11 +4,16 @@ import static org.assertj.core.api.AssertionsForInterfaceTypes.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
+import java.util.Map;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import com.javabom.toby.chapter7.내장형_db.DbConfig;
 import com.javabom.toby.chapter7.내장형_db.User;
@@ -17,6 +22,9 @@ import com.javabom.toby.chapter7.내장형_db.User;
 class H2DbTest {
 	@Autowired
 	JdbcTemplate jdbcTemplate;
+
+	@Autowired
+	TransactionTemplate transactionTemplate;
 
 	@Test
 	void test() {
@@ -27,4 +35,33 @@ class H2DbTest {
 			() -> assertThat(results).contains(new User("jaeyeon.seo", "서재연"))
 		);
 	}
+
+	@DisplayName("트랜잭션")
+	@Test
+	void transaction() {
+		try {
+			transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+				@Override
+				protected void doInTransactionWithoutResult(TransactionStatus status) {
+					updateSql("aaa", "bbb");
+					updateSql("ccc", "ccc"); // exception
+				}
+			});
+		} catch (RuntimeException ex) {
+			// ignore
+		}
+
+		// aaa 조회
+		List<Map<String, Object>> user = jdbcTemplate.queryForList("SELECT* FROM TEST_USER WHERE ID = 'aaa'");
+
+		assertThat(user).isEmpty();
+	}
+
+	private void updateSql(String id, String name) {
+		if (id.equals("ccc")) {
+			throw new RuntimeException();
+		}
+		jdbcTemplate.execute(String.format("INSERT INTO TEST_USER VALUES('%s', '%s')", id, name));
+	}
+
 }
